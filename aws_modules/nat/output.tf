ab096ea3a0718b852a41cd9450af28d287bd0f7f
protechanalysis <adewunmioluwaseyi98@.com>
2025-07-24 12:20:59 +0100

    update nat

--- aws_modules/nat/output.tf
+++ aws_modules/nat/output.tf
@@ -1,7 +1,7 @@
-output "nat_gateway_id" {
+output "nat_id" {
   value = aws_nat_gateway.nat_gateway.id
 }
 
-output "nat_gateway_public_ip" {
+output "nat_public_ip" {
   value = aws_nat_gateway.nat_gateway.public_ip
 }
